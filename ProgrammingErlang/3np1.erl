ee28acb29800d5912339be6a9d0f5eaedab7a4b2
Eung-ju PARK <eungju@gmail.com>
2007-04-29 01:16:48 +0900

    Implement pmax_cycle.

--- ProgrammingErlang/3np1.erl
+++ ProgrammingErlang/3np1.erl
@@ -1,5 +1,6 @@
 -module('3np1').
 -include_lib("eunit/include/eunit.hrl").
+-export([max_cycle/1, pmax_cycle/1]).
 
 cycle(N) ->
     cycle(N, 0).
@@ -17,6 +18,14 @@ cycle_test_() ->
     [?_assertMatch(1, cycle(1)),
      ?_assertMatch(2, cycle(2)),
      ?_assertMatch(6, cycle(5))].
+
+max_cycle(M) ->
+    lists:max(lists:map(fun cycle/1, lists:seq(1, M))).
+
+max_cycle_test_() ->
+    [?_assertMatch(1, max_cycle(1)),
+     ?_assertMatch(2, max_cycle(2)),
+     ?_assertMatch(20, max_cycle(10))].
 
 reduce(L) ->
     reduce(L, 0).
@@ -44,3 +53,35 @@ reduce_test_() ->
      ?_assertMatch(2, reduce([1, 2])),
      ?_assertMatch(2, reduce([2, 1]))
     ].
+
+worker() ->
+    receive
+	{From, N} ->
+	    From ! {N, cycle(N)},
+	    worker();
+	stop ->
+	    void
+    end.
+
+worker_test() ->
+    Pid = spawn(fun worker/0),
+    Pid ! {self(), 1},
+    Pid ! stop,
+    Result = receive
+		 {1, 1} ->
+		     true;
+		 _ ->
+		     false
+	     end,
+    ?assertMatch(true, Result).
+
+pmax_cycle(M) ->
+    L = lists:seq(1, M),
+    NWorkers = 10,
+    Workers = lists:map(fun(_) -> spawn(fun worker/0) end, lists:seq(1, NWorkers)),
+    lists:map(fun(N) -> lists:nth((N rem NWorkers) + 1, Workers) ! {self(), N} end, L),
+    lists:map(fun(Worker) -> Worker ! stop end, Workers),
+    reduce(L).
+    
+pmax_cycle_test() ->
+    ?assertMatch(20, pmax_cycle(10)).
