28de344634e010b2ec0e684d1c12f1dce6e9a792
Eung-ju PARK <eungju@gmail.com>
2007-05-06 23:53:23 +0900

    Maximum 3n + 1 cycle length.

--- ProgrammingErlang/3np1.hs
+++ ProgrammingErlang/3np1.hs
@@ -5,11 +5,21 @@ distributeFairly nelements nchunk = luckyOnes ++ unluckyOnes
       luckyOnes = (replicate remainder (atLeast + 1))
       unluckyOnes = (replicate (nchunk - remainder) atLeast)
 
-d2 nelements nchunk = map plus (take nchunk (zip ((take r ones) ++ zeros) (repeat d)))
+distributeFairly' nelements nchunk = map plus (take nchunk (zip ((take r ones) ++ zeros) (repeat d)))
     where
       r = (rem nelements nchunk)
       d = (div nelements nchunk)
       zeros = repeat 0
       ones = repeat 1
       plus (a, b) = a + b
+
+step n = step n 1
+    where
+      step 1 acc = acc
+      step a acc | (rem a 2) == 0 = step (div a 2) (acc + 1)
+                 | otherwise = step (3 * a + 1) (acc + 1)
+
+maximum_step xs = maximum (map step xs)
+
+main = putStrLn (show (maximum_step [1..1000000]))
 
