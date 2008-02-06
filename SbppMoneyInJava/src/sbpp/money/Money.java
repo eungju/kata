347d1b22b623c8c88968280578c5b5f006d8f9d9
Eung-ju PARK <eungju@gmail.com>
2008-02-06 16:04:01 +0900

    Start-2.

--- SbppMoneyInJava/src/sbpp/money/Money.java
+++ SbppMoneyInJava/src/sbpp/money/Money.java
@@ -3,4 +3,9 @@ package sbpp.money;
 public class Money {
 	private Number amount;
 	private String currency;
+	
+	public Money(Number amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 }
