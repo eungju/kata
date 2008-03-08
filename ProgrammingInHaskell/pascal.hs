4a3d03f0e786de46d18508d852787b2d5c35846d
Eung-ju PARK <eungju@gmail.com>
2008-03-08 19:29:42 +0900

    Wikipedia.

--- ProgrammingInHaskell/pascal.hs
+++ ProgrammingInHaskell/pascal.hs
@@ -1,5 +1,17 @@
+a :: Int -> Int -> Int
+a n k | k == 1 = 1
+      | n == k = 1
+      | otherwise = a (n - 1) (k - 1) + a (n - 1) k
+
+f :: Int -> Int
+f n = product [1..n] 
+
+(!) :: Int -> Int -> Int
+n ! k = div (f n) ((f k) * (f (n - k))) 
+
+a' :: Int -> Int -> Int
+a' n k = (n - 1) ! (k - 1)
+
 triangle :: Int -> [[Int]]
-triangle n = reverse foldl (\_ acc -> next ((head acc):acc)) [[1]] [1..n]
-    where
-      next :: [Int] -> [Int]
-      next s = foldl (\a (b:bs) -> (a:a+b:bs)) [0] s
+triangle h = map (\n -> map (a' n) [1..n]) [1..h]
+
