b6b08bdfe72b09e21e29df56d869226c97cf13c2
Eung-ju PARK <eungju at gmail dot com>
2008-10-14 01:00:03 +0900

    Use n = a + 2b + 3c + d.

--- counting.erl
+++ counting.erl
@@ -19,3 +19,16 @@ count(N, Trial, Count) ->
 	true ->
 	    lists:foldl(fun(X, S) -> count(N, [X|Trial], S) end, Count, lists:seq(1, 4))
     end.
+
+fac(N) ->
+    fac(N, 1).
+fac(0, Acc) ->
+    Acc;
+fac(N, Acc) ->
+    fac(N - 1, N * Acc).
+
+combination(A, B, C, D) ->
+    fac(A + B + C + D) div (fac(A) * fac(B) * fac(C) * fac(D)).
+
+count_fast(N) ->
+    lists:sum([combination(A, B, C, D) || A <- lists:seq(0, N), B <- lists:seq(0, N - A), C <- lists:seq(0, N - A - B), D <- lists:seq(0, N - A - B - C), (A + 2 * B + 3 * C + D) =:= N]).
