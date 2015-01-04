794480e4b4226d7aefb53ad56bd8cf784aed6b49
Park Eungju <eungju@gmail.com>
2015-01-04 19:50:03 +0900

    상태 저장 방식은 속도에 영향 없다.

--- Algospot/NQUEEN.hs
+++ Algospot/NQUEEN.hs
@@ -11,13 +11,12 @@ safeDiagonal cs c = all (uncurry (/=)) $ zip cs [c-1,c-2..0] ++ zip cs [c+1..]
 safe :: [Int] -> Int -> Bool
 safe cs c = safeVertical cs c && safeDiagonal cs c
 
-solve :: Int -> [[Int]] -> Int -> Int
-solve _ [] acc = acc
-solve n (cs:bs) acc | n == length cs = solve n bs (acc + 1)
-                    | otherwise = solve n ([(c:cs) | c <- [0..n-1], safe cs c] ++ bs) acc
+solve :: Int -> [Int] -> Int
+solve n cs | n == length cs = 1
+           | otherwise = sum [solve n (c:cs) | c <- [0..n-1], safe cs c]
 
 countSolutions :: Int -> Int
-countSolutions n = solve n [[i] | i <- [0..n-1]] 0
+countSolutions n = solve n []
 
 main :: IO ()
 main = do
