24c652317a7f918a06c482bafe4dac867a53677b
Park Eungju <eungju@gmail.com>
2015-01-13 12:39:46 +0900

    상 받을 수 있는 최대수.

--- CodeDojo/manyprizes.scala
+++ CodeDojo/manyprizes.scala
@@ -4,15 +4,41 @@ import org.specs2.mutable._
 
 case class ManyPrizes(n: Int) {
   def max_losts_for(p: Long): Int = {
-    ((n - 1) to 0 by -1).map((i: Int) => {
-      ((n - 1) to i by -1) map (math.pow(2, _).toInt) sum
+    (0 to (n - 1)).map((i: Int) => {
+      (i to (n - 1)) map (math.pow(2, _).toLong) sum
     }).count(_ < p)
   }
-  def max_number_for_lost(l: Int): Int = {
-    math.min(math.pow(2, l + 1).toInt - 2, math.pow(2, n).toInt - 1)
+  def max_number_for_lost(l: Int): Long = {
+    math.min(math.pow(2, l + 1).toLong - 2, math.pow(2, n).toLong - 1)
   }
-  def max_number_for_granted(p: Int): Int = {
+  def pessimistic_max_number_for(p: Long): Long = {
     max_number_for_lost(max_losts_for(p))
+  }
+  def min_wins_for(p: Long): Int = {
+    (0 to (n - 1)).map((i: Int) => {
+      (0 to i) map (math.pow(2, _).toLong) sum
+    }).count(_ >= p)
+  }
+  def max_number_for_win(w: Int): Long = {
+    math.pow(2, n).toLong - math.pow(2, w).toLong
+  }
+  def optimistic_max_number_for(p: Long): Long = {
+    max_number_for_win(min_wins_for(p))
+  }
+}
+
+object ManyPrizes {
+  def main(args: Array[String]): Unit = {
+    val t = readLine().toInt
+    (1 to t).foreach { i =>
+      val tc: Array[String] = readLine().split(' ')
+      val n = tc(0).toInt
+      val p = tc(1).toLong
+      val tournament = ManyPrizes(n)
+      val max_p = tournament.pessimistic_max_number_for(p)
+      val max_o = tournament.optimistic_max_number_for(p)
+      println(s"Case #${i}: ${max_p} ${max_o}")
+    }
   }
 }
 
@@ -33,16 +59,44 @@ class ManyPrizesSpec extends Specification {
     "6 for 2" in { n3.max_number_for_lost(2) must_== 6 }
     "7 for 3" in { n3.max_number_for_lost(3) must_== 7 }
   }
-  "max_number_for_granted" should {
+  "pessimistic_max_number_for" should {
     val n3 = ManyPrizes(3)
     "p=4" in {
-      ManyPrizes(3).max_number_for_granted(4) must_== 0
+      n3.pessimistic_max_number_for(4) must_== 0
     }
     "p=5" in {
-      ManyPrizes(3).max_number_for_granted(5) must_== 2
+      n3.pessimistic_max_number_for(5) must_== 2
     }
     "p=3" in {
-      ManyPrizes(3).max_number_for_granted(3) must_== 0
+      n3.pessimistic_max_number_for(3) must_== 0
+    }
+  }
+  "min_wins_for" should {
+    val n3 = ManyPrizes(3)
+    "0 for 8" in { n3.min_wins_for(8) must_== 0 }
+    "1 for 7" in { n3.min_wins_for(7) must_== 1 }
+    "1 for 4" in { n3.min_wins_for(4) must_== 1 }
+    "2 for 3" in { n3.min_wins_for(3) must_== 2 }
+    "2 for 2" in { n3.min_wins_for(2) must_== 2 }
+    "3 for 1" in { n3.min_wins_for(1) must_== 3 }
+  }
+  "max_number_for_win" should {
+    val n3 = ManyPrizes(3)
+    "0 for 3" in { n3.max_number_for_win(3) must_== 0 }
+    "4 for 2" in { n3.max_number_for_win(2) must_== 4 }
+    "6 for 1" in { n3.max_number_for_win(1) must_== 6 }
+    "7 for 0" in { n3.max_number_for_win(0) must_== 7 }
+  }
+  "optimistic_max_number_for" should {
+    val n3 = ManyPrizes(3)
+    "p=4" in {
+      ManyPrizes(3).optimistic_max_number_for(4) must_== 6
+    }
+    "p=5" in {
+      ManyPrizes(3).optimistic_max_number_for(5) must_== 6
+    }
+    "p=3" in {
+      ManyPrizes(3).optimistic_max_number_for(3) must_== 4
     }
   }
 }
